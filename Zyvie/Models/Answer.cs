eb373f6ef6416b74ed29e95b47ea7c48027d9662
Алексанндр Черношей < >
2015-11-28 14:04:40 +0300

    + add Models

---
+++ Zyvie/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zyvie.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public bool Right { get; set; }
+    }
+}
